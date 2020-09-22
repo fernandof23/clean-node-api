7b62c0e5702fae3959c0d83b973682ae588dfa1e
Fernando Santos <fernandorato.0@hotmail.com>
2020-09-22 16:50:35 -0300

    Move loginRouter to its own File

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,44 +1,7 @@
 /* eslint-disable max-classes-per-file */
 
-class MissingParamError extends Error {
-  constructor(paramName) {
-    super(`Missing param: ${paramName}`);
-    this.name = 'MissingParamError';
-  }
-}
-
-class HttpResponse {
-  static badRequest(paramName) {
-    return {
-      statusCode: 400,
-      body: new MissingParamError(paramName),
-    };
-  }
-
-  static serverError() {
-    return {
-      statusCode: 500,
-    };
-  }
-}
-
-class LoginRouter {
-  route(httpRequest) {
-    if (!httpRequest || !httpRequest.body) {
-      return HttpResponse.serverError();
-    }
-
-    const { email, password } = httpRequest.body;
-    if (!email) {
-      return HttpResponse.badRequest('email');
-    }
-    if (!password) {
-      return HttpResponse.badRequest('password');
-    }
-
-    return [];
-  }
-}
+const LoginRouter = require('./login-router');
+const MissingParamError = require('../helper/missing-param-error');
 
 describe('Login Router', () => {
   test('Should return 400 if no email is provided', () => {
